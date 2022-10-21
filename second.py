97bab156eb07302d7866f4c7c64717204b34e1b4
m.tatybaev <m.tatybaev@qiwi.uz>
2022-10-21 21:46:09 +0500

    Ломаем градиентный спуск

--- second.py
+++ second.py
@@ -1,9 +1,10 @@
 weight =0.5 
-goal_pred = 0.8 
-input = 0.5
+goal_pred =0.8 
+input =0.5
 for iteration in range(20):
     pred = input * weight
-    error = (pred - goal_pred) ** 2 
-    direction_and_amount = (pred - goal_pred) * input 
-    weight = weight - direction_and_amount
+    error = (pred - goal_pred) ** 2
+    delta = pred - goal_pred
+    weight_delta = input * delta
+    weight = weight - weight_delta
     print("Error:" + str(error) + " Prediction:" + str(pred))

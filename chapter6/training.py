2fb0f6317b8019df55624d19fb150eb568569caa
m.tatybaev <m.tatybaev@qiwi.uz>
2022-10-22 03:05:22 +0500

    Chapter 6 modified

---
+++ chapter6/training.py
@@ -0,0 +1,29 @@
+"""Обучение на полном наборе данных"""
+
+"""Мы обучили нейронную сеть распознавать одну комбинацию сигналов, но нам нужно, чтобы она распознавала все комбинации"""
+
+import numpy as np
+weights = np.array([0.5,0.48,-0.7]) 
+alpha = 0.1
+streetlights = np.array([
+    [ 1, 0, 1 ], 
+    [ 0, 1, 1 ], 
+    [ 0, 1, 1 ], 
+    [ 1, 1, 1 ], 
+    [ 0, 1, 1 ],
+    [ 1, 0, 1 ] 
+]) 
+walk_vs_stop = np.array( [0, 1, 0, 1, 1, 0] )
+input = streetlights[0] # [1,0,1]
+goal_prediction = walk_vs_stop[0] # Содержит0(стоять)
+for iteration in range(40): 
+    error_for_all_lights = 0
+    for row_index in range(len(walk_vs_stop)):
+        input = streetlights[row_index] 
+        goal_prediction = walk_vs_stop[row_index]
+        prediction = input.dot(weights)
+        error = (goal_prediction - prediction) ** 2 
+        error_for_all_lights += error
+        delta = prediction - goal_prediction
+        weights = weights - (alpha * (input * delta)) 
+        print("Prediction: " + str(prediction), "\t Error: " + str(error_for_all_lights))
